44d478e4db2c388acdd379c908e115484a690904
Maria Campos <campos.maria04@outlook.com>
2023-05-04 16:07:48 -0500

    Block 17 parts 1-6

--- coffee_data.js
+++ coffee_data.js
@@ -1,20 +1,11 @@
-// Prompt 1:
-// Clean the coffee_data file:
-
-// Checklist:
-// a. All prices should be numbers.
-// b. All items should be strings.
-// c. Objects and properties should have commas seperating them.
-// d. Connect the coffee_data.js file to the index.js file.
-
 const coffeeMenu = [
     {
         name: "cappuccino",
-        price: "8",
+        price: 8,
         seasonal: false,
     },
     {
-        name: espresso,
+        name: "espresso",
         price: 5,
         seasonal: false,
     },
@@ -24,28 +15,28 @@ const coffeeMenu = [
         seasonal: false,
     },
     {
-        name: affogato,
-        price: "9",
+        name: "affogato",
+        price: 9,
         seasonal: true,
     }, 
     {
         name: "macchiato",
-        price: "6",
+        price: 6,
         seasonal: false,
     },
     {
-        name: americano,
+        name: "americano",
         price: 7,
         seasonal: false,
     },
     {
         name: "iced coffee",
-        price: "6",
+        price: 6,
         seasonal: false,
     },
     {
-        name: frappe,
-        price: "8",
+        name: "frappe",
+        price: 8,
         seasonal: false,
     },
     {
@@ -56,3 +47,5 @@ const coffeeMenu = [
 ]
 
 module.exports = coffeeMenu;
+
+
--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-const coffeeMenu = require("./coffee_data");
+const coffeeMenu = require("./coffee_data.js");
+
+
+//Print an array of all the drinks on the menu.
+const drinks = (item) => {
+    return item.name;
+}
+console.table(coffeeMenu.map(drinks));
+
+//Print an array of drinks that cost 5 and under
+const underFive = (item) => {
+    return item.price <= 5;
+}
+const drinksUnderFive = coffeeMenu.filter(underFive);
+console.table(drinksUnderFive);
+
+
+// 4. Print an array of drinks that are priced at an even number.
+const evenNumber = (item) => {
+    return item.price % 2 == 0;
+}
+const even = coffeeMenu.filter(evenNumber);
+console.table(even);
+
+
+
+// 5. Print the total if you were to order one of every drink.
+const drinkPrice = (array) => {
+    return array.price;
+}
+const priceArray = coffeeMenu.map(drinkPrice);
+
+const total = (accumulator, currentValue) => {
+    return accumulator + currentValue;
+}
+const totalPrice = priceArray.reduce(total);
+console.table(totalPrice);
+
+
+
+// 6. Print an array with all the drinks that are seasonal.
+const seasonalDrinks = (item) => {
+    return item.seasonal === true;
+}
+const seasonal_ = coffeeMenu.filter(seasonalDrinks);
+console.table(seasonal_);
+
+
+// 7. Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
+const seasonal_Drinks = (item) => {
+    if (item.seasonal === true) {
+        return item.name + "with";
+    }
+}
+const _seasonal = coffeeMenu.filter(seasonal_Drinks);
+console.log(_seasonal);
